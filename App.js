8f74b26a900099270ea124894030282f28d50672
Santhosh Reddy <santhoor721@gmail.com>
2024-11-28 13:25:24 +0530

    changes in html and css and java

--- App.js
+++ App.js
@@ -1,4 +1,15 @@
-  let currentIndex = 0;
+function toggleMenu() {
+  const mobileNav = document.getElementById("mobileNav");
+  if (
+    mobileNav.style.display === "none" ||
+    mobileNav.style.display === ""
+  ) {
+    mobileNav.style.display = "block";
+  } else {
+    mobileNav.style.display = "none";
+  }
+}
+let currentIndex = 0;
   const items = document.querySelectorAll('.carousel-item');
   const totalItems = items.length;
 
